c7000ff3efd41266191bb82e7bf0280eee177f23
Audrey Acken <audreyacken@Audreys-MacBook-Pro-3.local>
2023-09-16 16:09:12 -0400

    integrate openai

--- backend/generate_response.py
+++ backend/generate_response.py
@@ -1,33 +1,8 @@
-﻿from flask import Flask, request, jsonify
-from PIL import Image
-import pytesseract
-from flask_cors import CORS
-
 import openai
 import os
 
-API_KEY = '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'
+API_KEY = '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'
 
-app = Flask(__name__)
-
-CORS(app)
-
-cors = CORS(app, resources={r"/upload": {"origins": ["http://localhost:3000"]}})
-
-@app.route('/upload', methods=['POST'])
-def upload_image():
-    try:
-        print("here!")
-        image = request.files['image']
-        img = Image.open(image)
-        ocr_text = pytesseract.image_to_string(img)
-        print(ocr_text)
-        response_text = generate_response(ocr_text, 'text_files/examples.txt', 'text_files/text_guidelines.txt')
-        print(response_text)
-        return jsonify({'text': response_text})
-    except Exception as e:
-        print(e)
-        return jsonify({'error': str(e)})
 
 def generate_response(test_message, examples, guidelines):
     openai.api_key = API_KEY
@@ -35,7 +10,10 @@ def generate_response(test_message, examples, guidelines):
     model = 'text-davinci-003'
     print("TESTTTTT")
     print(os.getcwd())
-    test_message_output = test_message
+    with open(test_message) as f:
+        test_message_output = f.read()
+        print("TTEST")
+    f.close()
     with open(examples) as f:
         examples_output = f.read()
     f.close()
@@ -45,6 +23,8 @@ def generate_response(test_message, examples, guidelines):
      
     print(test_message_output)
     
+
+
     response_prompt = '''
         An image of a medical pill bottle and prescription was fed through an OCR package and produces the text on the label. 
         Based on these snippets of text, output a simple, straightforward description of the medicine and it's uses. 
@@ -53,6 +33,7 @@ def generate_response(test_message, examples, guidelines):
         
     ''' + examples_output + "\n\nUse these guidelines to create the text response: \n" + guidelines_output + "\n\nFind a response for this conversation: \n" + test_message_output
     
+    print(response_prompt)
     response = openai.Completion.create(
         prompt=response_prompt,
         model=model,
@@ -60,11 +41,20 @@ def generate_response(test_message, examples, guidelines):
         temperature=0.95,
         n=1,
     )
+    print(response)
 
+    f = open('medication-reader-openai/text_files/results.txt', 'w')
+    f.write('')
+    f = open('medication-reader-openai/text_files/results.txt', 'a')
+    # change: display to front end
     result_array = []
     for result in response.choices:
+        f.write(result.text)
         result_array.append(result.text)
 
+    # formatted_str = formatArray(result_array)
+    # print(formatted_str)
+    # return formatted_str
     formatted_str = ""
     for r in result_array:
         formatted_str += r
@@ -72,5 +62,29 @@ def generate_response(test_message, examples, guidelines):
     return formatted_str
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+def txtToString(file_name):
+    with open(file_name) as f:
+        file = f.readlines()
+    str_txt = ""
+    for line in file:
+        str_txt += line
+    return str_txt
+
+
+def formatArray(array):
+    formatted_messages = []
+    for element in array:
+        try:
+            splitstring = element.split('Person 2: ')
+            formatted_messages.append(splitstring[1])
+        except:
+            print(element + " is not valid")
+        else:
+            print("continue")
+
+    str_txt = ""
+    for element in formatted_messages:
+        str_txt += element + "\n"
+    return str(str_txt)
+
+generate_response('medication-reader-openai/text_files/test_message.txt', 'medication-reader-openai/text_files/examples.txt', 'medication-reader-openai/text_files/text_guidelines.txt')
